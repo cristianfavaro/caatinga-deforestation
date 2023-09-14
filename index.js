9ea9aff31f52e4edfec8abd43b0956f447449729
crisfavaro <cristian.favaro@valor.com.br>
2023-09-14 00:24:47 -0300

    testando listeners

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
-import entry from "./src/entry";
+// using d3 for convenience
+var main = d3.select("main");
+var scrolly = main.select("#scrolly");
+var figure = scrolly.select("figure");
+var article = scrolly.select("article");
+var step = article.selectAll(".step");
 
-export default entry;
+// initialize the scrollama
+var scroller = scrollama();
+
+class Listener{
+    constructor() {
+        this.stepEnterListeners = [];
+        this.stepExitListeners = [];
+    };
+
+    addStepEnter(listener){
+        this.stepEnterListeners.push(listener)
+    };
+
+    addStepExit(listener){
+        this.stepExitListeners.push(listener)
+    };
+};
+
+const listeners = new Listener();
+
+
+// generic window resize listener event
+function handleResize() {
+    // 1. update height of step elements
+    var stepH = Math.floor(window.innerHeight * 0.75);
+    step.style("height", stepH + "px");
+
+    var figureHeight = window.innerHeight / 2;
+    var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+
+    figure
+        .style("height", figureHeight + "px")
+        .style("top", figureMarginTop + "px");
+
+    // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+}
+
+
+// scrollama event handlers
+listeners.addStepEnter( response => {
+    console.log(response);
+    // response = { element, direction, index }
+
+    // add color to current step only
+    step.classed("is-active", function (d, i) {
+        return i === response.index;
+    });
+
+    // update graphic based on step
+    figure.select("p").text(response.index + 1);
+})
+
+
+
+listeners.addStepExit( response => {
+    console.log('passeio step exit enter', response)
+})
+
+
+function init() {
+
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize();
+
+    // 2. setup the scroller passing options
+    // 		this will also initialize trigger observations
+    // 3. bind scrollama event handlers (this can be chained like below)
+    scroller
+        .setup({
+            step: "#scrolly article .step",
+            offset: 0.33,
+            debug: true
+        })
+        .onStepEnter(response => listeners.stepEnterListeners.map(listener => {
+            listener(response)
+        }))
+        .onStepExit(response => listeners.stepExitListeners.map(listener => {
+            listener(response)
+        }))
+}
+
+// kick things off
+åinit();
